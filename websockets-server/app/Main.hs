ac198ca693b6ffb17369a954ec6e0b4191d77ae4
chrislample <clample@crimson.ua.edu>
2017-02-27 12:40:56 -0500

    Configure websockets client and server
    The websockets client follows the halogen example shown here: https://github.com/slamdata/purescript-halogen/tree/6420fa5ed11d9bfe09afa896032027618411db30/examples/driver-websockets
    
    The websockets server is a simple server that echoes incoming messages.

--- websockets-server/app/Main.hs
+++ websockets-server/app/Main.hs
@@ -3,4 +3,7 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = startChatServer config
+  where config = Config
+          { address = "127.0.0.1"
+          , port = 1855}
--- websockets-server/src/Lib.hs
+++ websockets-server/src/Lib.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( startChatServer
+    , Config(..)
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Network.WebSockets ( runServer
+                          , PendingConnection
+                          , acceptRequest
+                          , receiveData
+                          , sendTextData)
+import qualified Data.Text as T
+import Control.Monad (forever)
+
+data Config = Config
+              { address :: String
+              , port    :: Int
+              } deriving (Show, Eq)
+
+startChatServer :: Config -> IO ()
+startChatServer config =
+  runServer (address config) (port config) chatServer
+
+chatServer :: PendingConnection -> IO ()
+chatServer pendingConnection = do
+  connection <- acceptRequest pendingConnection
+  forever $ do
+    msg <- receiveData connection
+    sendTextData connection $ msg `T.append` "meow"
+    
