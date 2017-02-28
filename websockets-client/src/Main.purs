c1c34b08edf8a785acd1c3f3411e26915ab7c545
chrislample <clample@crimson.ua.edu>
2017-02-27 22:19:47 -0500

    Don't use coroutine for websocket listening
    It should be possible to implement a message listener without using
    coroutine. The result may be less powerful but seems to fit the use
    case and is more clear.

--- websockets-client/src/Main.purs
+++ websockets-client/src/Main.purs
@@ -2,13 +2,9 @@ module Main where
 
 import Prelude
 import Control.Coroutine as CR
-import Control.Coroutine.Aff as CRA
 import Control.Monad.Aff (Aff)
-import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Var (($=))
-import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
 import Halogen as H
 import Halogen.Aff as HA
@@ -17,26 +13,6 @@ import Log as Log
 import WebSocket as WS
 import Control.Monad.Aff.Console (CONSOLE, log)
 import Control.Monad.Eff.Class (liftEff)
-    
--- A producer coroutine that emits messages that arrive from the websocket.
-wsProducer
-  :: forall eff
-   . WS.Connection -> CR.Producer String (Aff (avar :: AVAR, err :: EXCEPTION, ws :: WS.WEBSOCKET | eff)) Unit
-wsProducer (WS.Connection socket) = CRA.produce \emit -> do
-  
-  socket.onmessage $= \event -> do
-    emit $ Left $ WS.runMessage (WS.runMessageEvent event)
-
--- A consumer coroutine that takes the `query` function from our component IO
--- record and sends `AddMessage` queries in when it receives inputs from the
--- producer.
-wsConsumer
-  :: forall eff
-   . (Log.Query ~> Aff (HA.HalogenEffects eff))
-  -> CR.Consumer String (Aff (HA.HalogenEffects eff)) Unit
-wsConsumer query = CR.consumer \msg -> do
-  query $ H.action $ Log.AddMessage msg
-  pure Nothing
 
 messageSender
   :: forall eff
@@ -49,6 +25,17 @@ messageSender (WS.Connection socket) = CR.consumer \msg -> do
       liftEff $ socket.send (WS.Message msg')
   pure Nothing
 
+
+messageListener :: forall eff
+     . WS.Connection
+    -> (Log.Query ~> Aff (HA.HalogenEffects ( ws :: WS.WEBSOCKET | eff)))
+    -> Eff (HA.HalogenEffects (ws :: WS.WEBSOCKET | eff)) Unit
+messageListener (WS.Connection socket) query =
+  socket.onmessage $= \event -> do
+    let msg = WS.runMessage <<< WS.runMessageEvent $ event
+    HA.runHalogenAff <<< query <<< H.action <<< Log.AddMessage $  msg
+
+
 main :: Eff (HA.HalogenEffects (ws :: WS.WEBSOCKET, console :: CONSOLE)) Unit
 main = do
   connection <- WS.newWebSocket (WS.URL "ws://127.0.0.1:1855") []
@@ -57,7 +44,4 @@ main = do
     io <- runUI Log.component unit body
   
     io.subscribe $ messageSender connection
-    -- Connecting the consumer to the producer initializes both, opening the
-    -- websocket connection and feeding queries back to our component as messages
-    -- are received.
-    CR.runProcess (wsProducer connection CR.$$ wsConsumer io.query)
+    liftEff $ messageListener connection io.query       
